74c6c3246280be5233900a2c33ea6f3d4116684c
Vladimir <buyanov.vv@yandex.ru>
2023-12-13 20:15:30 +0700

    обавил решение 1 задачи

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,3 +1,25 @@
 ﻿// Задайте значения M и N. Напишите программу, 
 // которая выведет все натуральные числа в промежутке от M до N. 
 // Использовать рекурсию, не использовать циклы.
+
+void ShowRealNumbers(int currentNumber, int number)
+{   
+    if (currentNumber < number)
+    {
+        Console.Write(currentNumber + ", ");
+        ShowRealNumbers(currentNumber + 1, number);
+    }
+    if (currentNumber == number)
+    {
+        Console.Write(currentNumber);
+    }
+}
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (m<n) {
+    Console.Write($"M = {m}; N = {n} -> ");
+    ShowRealNumbers(m, n);
+} else { Console.WriteLine("Число M должно быть меньше числа N"); }
--- Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e11164294d6ce2e629008629705497bfa34b41dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
